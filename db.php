52489c13fdb7ee1c3ee12801a1baa81d46dbdd06
UBC Student <fponkia@students.cs.ubc.ca>
2024-09-01 12:07:59 -0700

    Added all the files needed for the website to run

--- db.php
+++ db.php
@@ -6,16 +6,16 @@ $fields = parse_url($uri);
 
 // build the DSN including SSL settings
 $conn = "mysql:";
-$conn .= "host=" . $fields["host"];
-$conn .= ";port=" . $fields["port"];;
+$conn .= "host=mysql-c4bd69a-joke-rating-website.g.aivencloud.com";
+$conn .= ";port=11937";
 $conn .= ";dbname=defaultdb";
 $conn .= ";sslmode=verify-ca;sslrootcert='D:/absolute/path/to/ssl/certs/ca.pem'";
 
 try {
-    $db = new PDO($conn, $fields["user"], $fields["pass"]);
+    $db = new PDO($conn, "avnadmin", "AVNS_adblRjpdBDLOf_-yb1D");
 
     $stmt = $db->query("SELECT VERSION()");
-    print($stmt->fetch()[0]);
+    // print($stmt->fetch()[0]);
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
