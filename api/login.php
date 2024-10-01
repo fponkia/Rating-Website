6e175396bc9296c156833ed0b23a3b18c6434cb3
UBC Student <fponkia@students.cs.ubc.ca>
2024-10-01 09:50:52 -0700

    Changed the session method to cookie

--- api/login.php
+++ api/login.php
@@ -56,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $query = null;
                 $result = null;
 
-                $_COOKIE("user_id", $row["user_id"], time() + 7200, "/");
+                setcookie("user_id", $row["user_id"], time() + 7200, "/");
                 header("Location: homepage1.php");
                 exit();
 
